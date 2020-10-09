dd6c92a3e61d36db4aea49f32ded2b9a8776c260
sayed94h <skazimi951@yahoo.com>
2020-10-09 23:45:43 +0200

    Finished the part display-delete-todo

--- src/init.js
+++ src/init.js
@@ -1,14 +1,15 @@
 "use strict";
 
-const runFunction = () => {
+window.onload = () => {
 	// set the initial state for your app
 	app.state = deepClone(initialState);
 	console.log("app:", app);
 
 	// render initial view and attach event listeners
-	const todosView = renderTodos(app._state.todos);
+	/* 	const todosView = renderTodos(app._state.todos);
 	todosView.addEventListener("change", toggleCompletedHandler); // event delegation!
-	document.getElementById("root").appendChild(todosView);
+	document.getElementById("root").appendChild(todosView); */
+	const todosView = view.displayTodos();
 
 	// log the initiation
 	logger.push({
@@ -18,4 +19,4 @@ const runFunction = () => {
 	});
 };
 
-runFunction();
+addBtn.addEventListener("click", handlers.addTodos);
